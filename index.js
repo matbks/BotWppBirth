c069fcd534b9964a9eab757ad5923dcb529ab700
Backhaus <44726485+matbks@users.noreply.github.com>
2023-04-28 20:44:25 -0300

    BotWppBirth1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mysql = require('mysql');
+require('dotenv').config();
+
 
 const connection = mysql.createConnection({
-    host: '89.116.225.101', // Ajuste o hostname ou IP do servidor MySQL
-    user: 'admin',
-    password: 'Redware@13042019',
-    database: 'redw_db'
+    host: process.env.DB_HOST , // Ajuste o hostname ou IP do servidor MySQL
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
   });
   
   
